ab72f7868b203fb69a66fe0a78e682abfd8514ab
Alejandro Mery <amery@jpi.io>
2021-09-06 18:16:18 +0100

    api/auth: add oauth2.Endpoint for MMF
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ api/auth/oauth2.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"golang.org/x/oauth2"
+)
+
+// Supported OAuth 2.0 Scopes
+type Scope string
+
+const (
+	BasicScope    Scope = "basic"
+	DownloadScope Scope = "download"
+)
+
+// Endpoint is MMF's OAuth 2.0 endpoint
+var Endpoint = oauth2.Endpoint{
+	AuthURL:   "https://auth.myminifactory.com/web/authorize",
+	TokenURL:  "https://auth.myminifactory.com/v1/oauth/tokens",
+	AuthStyle: oauth2.AuthStyleInHeader,
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/justprintit/mmf
 
 go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602
+	google.golang.org/appengine v1.6.7 // indirect
+)
