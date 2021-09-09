978e3e2afc18f682d3cb5b0863f03f43ae7ede84
Alejandro Mery <amery@jpi.io>
2021-09-09 02:17:01 +0100

    api/library: add initial library request executors
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- api/library/client.go
+++ api/library/client.go
@@ -1,6 +1,7 @@
 package library
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-resty/resty/v2"
@@ -12,6 +13,8 @@ const (
 
 type Client struct {
 	*resty.Client
+
+	TraceEnabled bool
 }
 
 func newClient(rc *resty.Client) *Client {
@@ -35,4 +38,42 @@ func NewWithClient(hc *http.Client) *Client {
 func NewWithTransport(transport http.RoundTripper) *Client {
 	rc := resty.New().SetTransport(transport)
 	return newClient(rc)
+}
+
+func (c *Client) R(referer string, args ...interface{}) *resty.Request {
+	req := c.Client.R()
+
+	// TraceInfo
+	if c.TraceEnabled {
+		req.EnableTrace()
+	}
+
+	// Referer
+	if len(args) > 0 {
+		referer = fmt.Sprintf(referer, args...)
+	}
+	if len(referer) == 0 {
+		referer = "/"
+	} else if referer[0] != '/' {
+		referer = "/" + referer
+	}
+	req.SetHeader("Referer", c.HostURL+referer)
+
+	return req
+}
+
+func (c *Client) J(referer string, args ...interface{}) *resty.Request {
+	req := c.R(referer, args...)
+	req.SetHeader("Accept", "application/json")
+	return req
+}
+
+func (c *Client) Head(library string) (*resty.Response, error) {
+	path := fmt.Sprintf("/data-library/%s", library)
+	return c.J("/library?v=%s", library).Head(path)
+}
+
+func (c *Client) Get(library string) (*resty.Response, error) {
+	path := fmt.Sprintf("/data-library/%s", library)
+	return c.J("/library?v=%s", library).Get(path)
 }
