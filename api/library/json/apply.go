4cac93041dc53507b63f17fe5db37651c6d47109
Alejandro Mery <amery@jpi.io>
2021-09-09 22:48:58 +0100

    api/library/types: Introduce Library{} placeholder and system to populate it
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ api/library/json/apply.go
@@ -0,0 +1,13 @@
+package json
+
+import (
+	"github.com/justprintit/mmf/api/library/types"
+)
+
+func (w *Users) Apply(d *types.Library) error {
+	return nil
+}
+
+func (w *Objects) Apply(d *types.Library) error {
+	return nil
+}
---
+++ api/library/types.go
@@ -0,0 +1,9 @@
+package library
+
+import (
+	"github.com/justprintit/mmf/api/library/types"
+)
+
+type (
+	Library = types.Library
+)
