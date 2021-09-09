2c51b9cd882b5e3c36810d68d0b1729ee2f165ea
Alejandro Mery <amery@jpi.io>
2021-09-09 21:55:00 +0100

    api/library/json: improve Group.Id dynamic type
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- api/library/json/types.go
+++ api/library/json/types.go
@@ -24,11 +24,34 @@ type Groups struct {
 }
 
 type Group struct {
-	Id       json.Number
+	Id       GroupId
 	Name     string
 	Objects  int            `json:"total_count_objects,omitempty"`
 	Children []Group        `json:"childrens,omitempty"`
 	API      map[string]API `json:"apis,omitempty"`
+}
+
+type GroupId struct {
+	id int
+	s  string
+}
+
+func (w *GroupId) UnmarshalJSON(data []byte) error {
+	if data[0] == '"' {
+		w.id = 0
+		return json.Unmarshal(data, &w.s)
+	} else {
+		w.s = ""
+		return json.Unmarshal(data, &w.id)
+	}
+}
+
+func (w *GroupId) MarshalJSON() ([]byte, error) {
+	if len(w.s) > 0 {
+		return json.Marshal(w.s)
+	} else {
+		return json.Marshal(w.id)
+	}
 }
 
 type API struct {
