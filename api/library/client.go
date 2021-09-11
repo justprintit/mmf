48cae4c1a6b623d7dbec6a28d45901487558b47b
Alejandro Mery <amery@jpi.io>
2021-09-11 22:20:51 +0100

    api/library.Client: add DataDir field
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- api/library/client.go
+++ api/library/client.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-resty/resty/v2"
+	"go.sancus.dev/core/errors"
 
 	"github.com/justprintit/mmf"
 	"github.com/justprintit/mmf/api/client"
@@ -12,6 +13,8 @@ import (
 
 type Client struct {
 	client.Client
+
+	DataDir string
 }
 
 func (c *Client) Init(cred mmf.Credentials, rc *resty.Client) *Client {
@@ -36,7 +39,11 @@ func NewWithTransport(cred mmf.Credentials, transport http.RoundTripper) *Client
 func NewWithOptions(options ...client.ClientOption) (*Client, error) {
 	c := new(Client)
 	for _, opt := range options {
-		if err := opt.Apply(&c.Client); err != nil {
+		if lopt, ok := opt.(LibraryClientOption); ok {
+			if err := lopt.ApplyLibrary(c); err != nil {
+				return nil, err
+			}
+		} else if err := opt.Apply(&c.Client); err != nil {
 			return nil, err
 		}
 	}
@@ -49,6 +56,22 @@ func (c *Client) GetLibrary(library string) (*resty.Response, error) {
 	return c.J("/library?v=%s", library).Get(path)
 }
 
+type LibraryClientOption interface {
+	client.ClientOption
+
+	ApplyLibrary(*Client) error
+}
+
+type LibraryClientOptionFunc func(*Client) error
+
+func (f LibraryClientOptionFunc) ApplyLibrary(c *Client) error {
+	return f(c)
+}
+
+func (f LibraryClientOptionFunc) Apply(c *client.Client) error {
+	return errors.New("Invalid usage of %T", f)
+}
+
 func WithCredentials(cred mmf.Credentials) client.ClientOption {
 	return client.WithCredentials(cred)
 }
@@ -59,4 +82,14 @@ func WithTransport(transport http.RoundTripper) client.ClientOption {
 
 func WithCookieJar(jar http.CookieJar) client.ClientOption {
 	return client.WithCookieJar(jar)
+}
+
+func WithDataDir(datadir string) client.ClientOption {
+	return LibraryClientOptionFunc(func(c *Client) error {
+		if datadir == "" {
+			datadir = "."
+		}
+		c.DataDir = datadir
+		return nil
+	})
 }
