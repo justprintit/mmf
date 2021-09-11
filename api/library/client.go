c60ddc0ee0b0de77c54a2c8903ff8dcae96e6b5d
Alejandro Mery <amery@jpi.io>
2021-09-11 22:03:49 +0100

    api/library: introduce NewWithOptions()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- api/library/client.go
+++ api/library/client.go
@@ -33,7 +33,30 @@ func NewWithTransport(cred mmf.Credentials, transport http.RoundTripper) *Client
 	return new(Client).Init(cred, rc)
 }
 
+func NewWithOptions(options ...client.ClientOption) (*Client, error) {
+	c := new(Client)
+	for _, opt := range options {
+		if err := opt.Apply(&c.Client); err != nil {
+			return nil, err
+		}
+	}
+	c.Client.Init(mmf.Credentials{}, nil)
+	return c, nil
+}
+
 func (c *Client) GetLibrary(library string) (*resty.Response, error) {
 	path := fmt.Sprintf("/data-library/%s", library)
 	return c.J("/library?v=%s", library).Get(path)
+}
+
+func WithCredentials(cred mmf.Credentials) client.ClientOption {
+	return client.WithCredentials(cred)
+}
+
+func WithTransport(transport http.RoundTripper) client.ClientOption {
+	return client.WithTransport(transport)
+}
+
+func WithCookieJar(jar http.CookieJar) client.ClientOption {
+	return client.WithCookieJar(jar)
 }
