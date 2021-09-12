d31b1fbe65b39ca65c83c0934ec17be39081307c
Alejandro Mery <amery@jpi.io>
2021-09-12 20:59:10 +0100

    api/library: extend RefreshLibraries() to get shared, purchases and pledges libraries
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- api/library/refresh.go
+++ api/library/refresh.go
@@ -4,6 +4,43 @@ import (
 	"context"
 )
 
-func (c *Client) RefreshLibraries(ctx context.Context) error {
+// Reload persistent Library data
+func (c *Client) Reload() error {
 	return nil
+}
+
+func (c *Client) RefreshLibraries(ctx context.Context) error {
+	// load persistent data
+	if err := c.Reload(); err != nil {
+		return err
+	}
+
+	// shared with me
+	c.Add(func(c *Client, ctx context.Context) error {
+		return c.RefreshSharedLibrary(ctx)
+	})
+	// purchases
+	c.Add(func(c *Client, ctx context.Context) error {
+		return c.RefreshPurchasesLibrary(ctx)
+	})
+	// pledges
+	c.Add(func(c *Client, ctx context.Context) error {
+		return c.RefreshPledgesLibrary(ctx)
+	})
+	return nil
+}
+
+func (c *Client) RefreshSharedLibrary(ctx context.Context) error {
+	_, err := c.GetSharedLibrary(ctx)
+	return err
+}
+
+func (c *Client) RefreshPurchasesLibrary(ctx context.Context) error {
+	_, err := c.GetSharedLibrary(ctx)
+	return err
+}
+
+func (c *Client) RefreshPledgesLibrary(ctx context.Context) error {
+	_, err := c.GetPledgesLibrary(ctx)
+	return err
 }
