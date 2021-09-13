1b8df2cce4838b738246dd2a4b1a362a0ca0d768
Alejandro Mery <amery@jpi.io>
2021-09-13 03:26:11 +0100

    api/library: extend RefreshLibraries() to pull all pages
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- api/library/refresh.go
+++ api/library/refresh.go
@@ -2,6 +2,9 @@ package library
 
 import (
 	"context"
+	"log"
+
+	"github.com/justprintit/mmf/api/library/json"
 )
 
 // Reload persistent Library data
@@ -30,17 +33,128 @@ func (c *Client) RefreshLibraries(ctx context.Context) error {
 	return nil
 }
 
+func (c *Client) refreshSharedLibrary(ctx context.Context, offset int, users ...json.User) error {
+	i := offset
+	for _, u := range users {
+		i++
+
+		if u.Name != u.Id {
+			log.Printf("User.%v: %s (%s)", i, u.Name, u.Id)
+		} else {
+			log.Printf("User.%v: %s", i, u.Id)
+		}
+
+	}
+	return nil
+}
+
 func (c *Client) RefreshSharedLibrary(ctx context.Context) error {
-	_, err := c.GetSharedLibrary(ctx)
-	return err
+	var p *Pagination
+
+	page := 1
+	ok := true
+
+	for ok {
+		// get json items
+		d, err := c.GetSharedLibraryPage(ctx, page)
+		if err != nil {
+			return err
+		}
+
+		if p == nil {
+			//  first page
+			p = c.Pages(len(d.User), d.Count)
+		}
+
+		// process in parallel
+		offset := p.Size * (page - 1)
+		c.Spawn(func(c *Client, ctx context.Context) error {
+			return c.refreshSharedLibrary(ctx, offset, d.User...)
+		})
+
+		// next page
+		page, _, ok = p.Next(page)
+	}
+
+	return nil
+}
+
+func (c *Client) refreshPurchasesLibrary(ctx context.Context, offset int, objects ...json.Object) error {
+	i := offset
+	for _, obj := range objects {
+		i++
+		log.Printf("%s.%v: %q (%v, %q)", "Purchase", i, obj.Name, obj.Id, obj.ObjType)
+	}
+	return nil
 }
 
 func (c *Client) RefreshPurchasesLibrary(ctx context.Context) error {
-	_, err := c.GetSharedLibrary(ctx)
-	return err
+	var p *Pagination
+
+	page := 1
+	ok := true
+
+	for ok {
+		// get json items
+		d, err := c.GetPurchasesLibraryPage(ctx, page)
+		if err != nil {
+			return err
+		}
+
+		if p == nil {
+			//  first page
+			p = c.PagesN(len(d.Items), d.Count)
+		}
+
+		// process in parallel
+		offset := p.Size * (page - 1)
+		c.Spawn(func(c *Client, ctx context.Context) error {
+			return c.refreshPurchasesLibrary(ctx, offset, d.Items...)
+		})
+
+		// next page
+		page, _, ok = p.Next(page)
+	}
+
+	return nil
+}
+
+func (c *Client) refreshPledgesLibrary(ctx context.Context, offset int, objects ...json.Object) error {
+	i := offset
+	for _, obj := range objects {
+		i++
+		log.Printf("%s.%v: %q (%v, %q)", "Pledge", i, obj.Name, obj.Id, obj.ObjType)
+	}
+	return nil
 }
 
 func (c *Client) RefreshPledgesLibrary(ctx context.Context) error {
-	_, err := c.GetPledgesLibrary(ctx)
-	return err
+	var p *Pagination
+
+	page := 1
+	ok := true
+
+	for ok {
+		// get json items
+		d, err := c.GetPledgesLibraryPage(ctx, page)
+		if err != nil {
+			return err
+		}
+
+		if p == nil {
+			//  first page
+			p = c.PagesN(len(d.Items), d.Count)
+		}
+
+		// process in parallel
+		offset := p.Size * (page - 1)
+		c.Spawn(func(c *Client, ctx context.Context) error {
+			return c.refreshPledgesLibrary(ctx, offset, d.Items...)
+		})
+
+		// next page
+		page, _, ok = p.Next(page)
+	}
+
+	return nil
 }
