16d2493637c2e8cd9cc8fce27ad4b2716a71016d
Alejandro Mery <amery@jpi.io>
2021-09-13 02:59:21 +0100

    api/library: add Pagination helper
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ api/library/pages.go
@@ -0,0 +1,35 @@
+package library
+
+import (
+	"github.com/justprintit/mmf/api/client/json"
+)
+
+type Pagination struct {
+	Size  int
+	Total int
+}
+
+func (p *Pagination) Next(prev int) (int, int, bool) {
+	offset := prev * p.Size
+	if offset < p.Total {
+		return prev + 1, offset, true
+	}
+	return 0, 0, false
+}
+
+func (c *Client) Pages(n, total int) *Pagination {
+	return &Pagination{
+		Size:  n,
+		Total: total,
+	}
+}
+
+func (c *Client) PagesN(n int, count json.Number) *Pagination {
+	total := n
+
+	if n64, err := count.Int64(); err == nil {
+		total = int(n64)
+	}
+
+	return c.Pages(n, total)
+}
