b8d3ca17d7e962000a1e858ea5a3d6c5f66cbdd8
Alejandro Mery <amery@jpi.io>
2021-09-16 22:12:48 +0100

    api/library: rework RefreshLibraries() to use the download manager
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- api/library/json.go
+++ api/library/json.go
@@ -8,16 +8,22 @@ import (
 	"github.com/go-resty/resty/v2"
 )
 
+func (c *Client) composeLibraryPageRequest(library string, page int) (string, string) {
+	path := fmt.Sprintf("/data-library/%s", url.PathEscape(library))
+	if page > 0 {
+		path = fmt.Sprintf("%s?page=%v", path, page)
+	}
+	referer := fmt.Sprintf("/library?v=%s", url.QueryEscape(library))
+	return referer, path
+}
+
 func (c *Client) GetLibrary(ctx context.Context, library string, out interface{}) (*resty.Response, error) {
 	return c.GetLibraryPage(ctx, library, 0, out)
 }
 
 func (c *Client) GetLibraryPage(ctx context.Context, library string, page int, out interface{}) (*resty.Response, error) {
-	path := fmt.Sprintf("/data-library/%s", url.PathEscape(library))
-	if page > 0 {
-		path = fmt.Sprintf("%s?page=%v", path, page)
-	}
-	req := c.J("/library?v=%s", url.QueryEscape(library))
+	referer, path := c.composeLibraryPageRequest(library, page)
+	req := c.J(referer)
 
 	if ctx != nil {
 		req.SetContext(ctx)
@@ -28,4 +34,15 @@ func (c *Client) GetLibraryPage(ctx context.Context, library string, page int, o
 	}
 
 	return req.Get(path)
+}
+
+// lazy shortcut for getting data without page= parameter
+func (c *Client) ScheduleLibraryRequest(library string, out interface{}, fn ResponseHandler) {
+	c.ScheduleLibraryPageRequest(library, 0, out, fn)
+}
+
+func (c *Client) ScheduleLibraryPageRequest(library string, page int, out interface{}, fn ResponseHandler) {
+	referer, path := c.composeLibraryPageRequest(library, page)
+
+	c.Download(referer, path, out, fn)
 }
