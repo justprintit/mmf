c46a93a050fc2a01f78b295f5cceba2d26bcc81f
Alejandro Mery <amery@jpi.io>
2021-09-23 01:19:56 +0100

    api/library/store/yaml: add persistent storage for Groups
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- api/library/store/yaml/dump.go
+++ api/library/store/yaml/dump.go
@@ -1,6 +1,7 @@
 package yaml
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"sort"
@@ -8,11 +9,20 @@ import (
 
 	"gopkg.in/yaml.v2"
 
+	"go.sancus.dev/core/errors"
+
 	"github.com/justprintit/mmf/api/library/types"
 )
 
+type ExportDepth int
+
+const (
+	ExportShallow ExportDepth = iota
+	ExportDeep
+)
+
 // Export User data for YAML encoding
-func (store *Store) ExportUser(w *types.User) (*User, error) {
+func (store *Store) ExportUser(w *types.User, depth ExportDepth) (*User, error) {
 	name := strings.TrimSpace(w.Name)
 	name = strings.ReplaceAll(name, string(os.PathSeparator), "-")
 
@@ -26,18 +36,80 @@ func (store *Store) ExportUser(w *types.User) (*User, error) {
 		Avatar:   w.Avatar,
 	}
 
+	if depth == ExportDeep {
+		var err error
+
+		u.Groups, err = store.ExportGroups(w.Groups, depth)
+		if err != nil {
+			return u, err
+		}
+	}
+
 	return u, nil
 }
 
+// Export Groups data for YAML encoding
+func (store *Store) ExportGroups(w []*types.Group, depth ExportDepth) ([]Group, error) {
+	var check errors.ErrorStack
+
+	groups := make([]Group, 0, len(w))
+
+	// groups
+	for _, g0 := range w {
+		g, err := store.ExportGroup(g0, depth)
+		if err != nil {
+			check.AppendError(err)
+		} else {
+			groups = append(groups, g)
+		}
+	}
+
+	// sorted
+	sort.Slice(groups[:], func(i, j int) bool {
+		return groups[i].Id < groups[j].Id
+	})
+
+	if !check.Ok() {
+		return groups, &check
+	}
+
+	return groups, nil
+}
+
+// Export Group data for YAML encoding
+func (store *Store) ExportGroup(w *types.Group, depth ExportDepth) (Group, error) {
+	name := strings.TrimSpace(w.Name)
+	name = strings.ReplaceAll(name, string(os.PathSeparator), "-")
+	if len(name) == 0 {
+		name = fmt.Sprintf("%v", w.Id)
+	}
+
+	g := Group{
+		Id:   w.Id,
+		Name: name,
+	}
+
+	if depth == ExportDeep && len(w.Subgroups) > 0 {
+		var err error
+
+		g.Subgroups, err = store.ExportGroups(w.Subgroups, depth)
+		if err != nil {
+			return g, err
+		}
+	}
+
+	return g, nil
+}
+
 // Export Library for YAML encoding
-func (store *Store) ExportSlice(data *types.Library) yaml.MapSlice {
+func (store *Store) ExportSlice(data *types.Library, depth ExportDepth) yaml.MapSlice {
 	n := len(data.User)                 // users count
 	keys := make([]string, 0, n)        // for sorting, lower case names
 	mkeys := make(map[string]*User, n)  // lower case name to user
 	slice := make([]yaml.MapItem, 0, n) // return value
 
 	for _, w := range data.User {
-		u, _ := store.ExportUser(w)
+		u, _ := store.ExportUser(w, depth)
 		key := strings.ToLower(u.Name)
 
 		keys = append(keys, key)
@@ -60,5 +132,5 @@ func (store *Store) ExportSlice(data *types.Library) yaml.MapSlice {
 
 // Write all data into one file
 func (store *Store) WriteTo(data *types.Library, out io.Writer) (int64, error) {
-	return WriteTo(store.ExportSlice(data), out)
+	return WriteTo(store.ExportSlice(data, ExportDeep), out)
 }
--- api/library/store/yaml/store.go
+++ api/library/store/yaml/store.go
@@ -22,9 +22,10 @@ type Store struct {
 }
 
 func (store *Store) Load() (*types.Library, error) {
-	const merge = false
+	var check errors.ErrorStack
 
-	check := &errors.ErrorStack{}
+	const unique = false // !merge
+
 	data := &types.Library{}
 
 	// for each user on Basedir
@@ -33,21 +34,29 @@ func (store *Store) Load() (*types.Library, error) {
 		return nil, err
 	}
 
-	n := len(files)
-	for i, filename := range files {
-		log.Printf("File[%v/%v]: %q", i, n, filename)
+	for _, filename := range files {
+		log.Printf("Load: %q", filename)
+
 		if u, err := store.ReadUserFile(filename); err != nil {
 			check.AppendWrapped(err, "LoadUserFile")
-		} else if _, err := data.AddUser(u, merge); err != nil {
+		} else if u, err := data.AddUser(u, unique); err != nil {
 			check.AppendWrapped(err, "AddUser(%q)", u.Username)
+		} else {
+			// Groups
+			name := filepath.Base(filename)
+			name = strings.TrimSuffix(name, filepath.Ext(name))
+
+			base := filepath.Join(store.Basedir, name)
+			if err := store.loadGroups(data, u, nil, base); err != nil {
+				check.AppendWrapped(err, "LoadGroups(%q)", u.Username)
+			}
 		}
 	}
 
-	if check.Ok() {
-		return data, nil // typed nil != nil
-	} else {
-		return data, check
+	if !check.Ok() {
+		return data, &check
 	}
+	return data, nil
 }
 
 func (store *Store) Store(data *types.Library) error {
@@ -60,6 +69,21 @@ func (store *Store) Store(data *types.Library) error {
 		if err := store.writeUser(base, v); err != nil {
 			check.AppendError(err)
 		}
+
+		// Groups
+		if len(v.Groups) > 0 {
+			base = filepath.Join(base, v.Name)
+
+			if err := os.MkdirAll(base, StoreDirectoryMode); err != nil {
+				check.AppendError(err)
+			} else {
+				for _, g := range v.Groups {
+					if err := store.writeGroups(base, g); err != nil {
+						check.AppendError(err)
+					}
+				}
+			}
+		}
 	}
 
 	if !check.Ok() {
@@ -71,7 +95,7 @@ func (store *Store) Store(data *types.Library) error {
 
 func (store *Store) writeUser(base string, user *types.User) error {
 	// prepare data
-	u, err := store.ExportUser(user)
+	u, err := store.ExportUser(user, ExportShallow)
 	if err != nil || u == nil {
 		return err
 	}
@@ -113,4 +137,111 @@ func (store *Store) ReadUserFile(filename string) (*types.User, error) {
 	}
 
 	return u, nil
+}
+
+func (store *Store) loadGroups(data *types.Library, u *types.User, parent *types.Group, base string) error {
+	var check errors.ErrorStack
+
+	const unique = false
+
+	// for each group on base
+	files, err := filepath.Glob(filepath.Join(base, "*.yaml"))
+	if err != nil {
+		return err
+	}
+
+	for _, filename := range files {
+		log.Printf("Load: %q", filename)
+
+		if g, err := store.ReadGroupFile(filename); err != nil {
+			check.AppendWrapped(err, "LoadGroupFile")
+		} else if g != nil {
+			if parent != nil {
+				g, err = parent.AddSubgroup(g, unique)
+			} else {
+				g, err = u.AddGroup(g, unique)
+			}
+
+			if err != nil {
+				check.AppendWrapped(err, "AddGroup(%v)", g.Id)
+			}
+		}
+	}
+
+	if !check.Ok() {
+		return &check
+	}
+	return nil
+}
+
+func (store *Store) ReadGroupFile(filename string) (*types.Group, error) {
+	// open file for reading
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	// decode
+	g := &types.Group{}
+	adaptor := NewDecoder(f)
+	if err = adaptor.Decode(g); err != nil {
+		return nil, err
+	}
+
+	if len(g.Name) == 0 {
+		// use filename as group name
+		base := strings.TrimSpace(filepath.Base(filename))
+		g.Name = strings.TrimSuffix(base, filepath.Ext(base))
+	}
+
+	return g, nil
+}
+
+func (store *Store) writeGroups(base string, group *types.Group) error {
+	// prepare data
+	g, err := store.ExportGroup(group, ExportShallow)
+	if err != nil || len(g.Name) == 0 {
+		return err
+	}
+
+	// output file
+	filename := filepath.Join(base, g.Name+".yaml")
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, StoreFileMode)
+	if err != nil {
+		return err
+	}
+
+	// encode
+	adapter := NewEncoder(f)
+	err = adapter.Encode(g)
+
+	adapter.Close()
+	f.Close()
+
+	if err != nil {
+		return err
+	}
+
+	// subgroups
+	if len(group.Subgroups) > 0 {
+		var check errors.ErrorStack
+
+		base = filepath.Join(base, g.Name)
+		if err := os.MkdirAll(base, StoreDirectoryMode); err != nil {
+			check.AppendError(err)
+		} else {
+			for _, sg := range group.Subgroups {
+				if err = store.writeGroups(base, sg); err != nil {
+					check.AppendError(err)
+				}
+			}
+		}
+
+		if !check.Ok() {
+			return &check
+		}
+	}
+
+	return nil
 }
