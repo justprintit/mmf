0553ab1d01995bbd54a0a19ef819515bc8405016
Alejandro Mery <amery@jpi.io>
2021-10-09 05:41:13 +0100

    cmd/mmf: introduce `mmf config` initialising config file if needed
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- cmd/mmf/config.go
+++ cmd/mmf/config.go
@@ -3,12 +3,18 @@ package main
 import (
 	"io"
 	"log"
+	"os"
+
+	"github.com/spf13/cobra"
 
 	"go.sancus.dev/config/flags"
 	"go.sancus.dev/config/yaml"
+	"go.sancus.dev/core/errors"
 
 	"github.com/justprintit/mmf/api"
 )
+
+const DefaultConfigFileMode os.FileMode = 0600 // owner only, because we include the password
 
 type AuthConfig struct {
 	User   api.Credentials `yaml:"user"`
@@ -35,4 +41,67 @@ func (c *Config) ReadInFile(filename string) error {
 
 func (c *Config) WriteTo(out io.Writer) (int64, error) {
 	return yaml.WriteTo(out, c)
+}
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: CmdName + " config manages the configuration file",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var (
+			write bool
+			dump  bool
+			check errors.ErrorStack
+		)
+
+		for _, x := range args {
+			switch x {
+			case "dump":
+				dump = true
+			default:
+				check.InvalidArgument(x)
+			}
+		}
+
+		if !cfg.Auth.Client.Empty() {
+			// client_id set
+		} else if err := cfg.Auth.Client.Init(); err != nil {
+			// failed to generate client_id
+			check.AppendError(err)
+		} else {
+			// client_id generated, rewrite config
+			write = true
+		}
+
+		if cfgReadError == nil {
+			// no error
+		} else if os.IsNotExist(cfgReadError) {
+			// no such file or directory, make one
+			write = true
+		} else {
+			// other read error
+			check.AppendError(cfgReadError)
+		}
+
+		if dump {
+			_, err := cfg.WriteTo(os.Stdout)
+			if err != nil {
+				check.AppendError(err)
+			}
+		}
+
+		if write {
+			_, err := yaml.WriteFile(cfgFile, cfg, DefaultConfigFileMode)
+			if err == nil {
+				log.Println(cfgFile, "written")
+			} else {
+				check.AppendError(err)
+			}
+		}
+
+		return check.AsError()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,8 @@ require (
 	github.com/deepmap/oapi-codegen v1.8.3
 	github.com/sethvargo/go-password v0.2.0
 	github.com/spf13/cobra v1.2.1
-	go.sancus.dev/config v0.6.1
+	go.sancus.dev/config v0.8.0
+	go.sancus.dev/core v0.16.0
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602
 )
