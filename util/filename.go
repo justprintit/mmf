7efe71296ce5fe24889d16057ddd81ecc1cc7089
Alejandro Mery <amery@jpi.io>
2021-10-09 21:20:50 +0100

    util: add Sanitize() to produce filesystem safe names
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ util/filename.go
@@ -0,0 +1,12 @@
+package util
+
+import (
+	"strings"
+)
+
+func Sanitize(s string) string {
+	s = strings.TrimSpace(s)
+	s = strings.ReplaceAll(s, "/", "-")
+	s = strings.ReplaceAll(s, "\\", "-")
+	return s
+}
