1d9f9ce2c00ca7a4815886ceb8657ab6918a64bd
Alejandro Mery <amery@jpi.io>
2021-10-11 00:20:13 +0100

    api/mmf.Client: rename ClientId to ClientID to follow oauth2.Config naming
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- api/mmf/config.go
+++ api/mmf/config.go
@@ -11,29 +11,29 @@ type User struct {
 }
 
 // for accessing the oauth2 API you first need to register
-// a generated ClientId at https://www.myminifactory.com/settings/dev
+// a generated ClientID at https://www.myminifactory.com/settings/dev
 // ClientSecret then arrives by email "You Have been Authorised to use the MyMiniFactory API"
 type Client struct {
-	ClientId     string `yaml:"client_key"`
+	ClientID     string `yaml:"client_key"`
 	ClientSecret string `yaml:"client_secret"`
 
 	AccessToken  string `yaml:"access_token,omitempty"`
 	RefreshToken string `yaml:"refresh_token,omitempty"`
 }
 
-// Empty() checks if client credentials aren't set
+// Empty() checks if Client credentials aren't set
 func (c *Client) Empty() bool {
-	return len(c.ClientId) == 0
+	return len(c.ClientID) == 0
 }
 
-// Init() generates a new ClientId if none is set
+// Init() generates a new ClientID if none is set
 func (c *Client) Init() error {
-	if len(c.ClientId) == 0 {
+	if len(c.ClientID) == 0 {
 		id, err := util.RandomString(16)
 		if err != nil {
 			return err
 		}
-		c.ClientId = id
+		c.ClientID = id
 	}
 	return nil
 }
