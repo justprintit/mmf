d9db53783c3db99f9ac75704cc8af231ff46da04
Alejandro Mery <amery@jpi.io>
2021-10-11 00:57:35 +0100

    api/mmf.Client: add method to generate a &oauth2.Config{}
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- api/mmf/config.go
+++ api/mmf/config.go
@@ -1,6 +1,10 @@
 package mmf
 
 import (
+	"golang.org/x/oauth2"
+
+	"go.sancus.dev/core/errors"
+
 	"github.com/justprintit/mmf/util"
 )
 
@@ -19,6 +23,48 @@ type Client struct {
 
 	AccessToken  string `yaml:"access_token,omitempty"`
 	RefreshToken string `yaml:"refresh_token,omitempty"`
+}
+
+// Ok() checks if the Client can be used to compose an oauth2.Config
+func (c *Client) Ok() bool {
+	if len(c.ClientID) > 0 && len(c.ClientSecret) > 0 {
+		return true
+	}
+	return false
+}
+
+// NewOauth2 creates a new oauth2.Config for use MMF's OpenAPI
+func (c *Client) NewOauth2(callback string) (*oauth2.Config, error) {
+	var (
+		id     string
+		secret string
+		check  errors.ErrorStack
+	)
+
+	if callback == "" {
+		check.MissingArgument("callback")
+	}
+
+	if id = c.ClientID; id == "" {
+		check.MissingField("client_key")
+	}
+
+	if secret = c.ClientSecret; secret == "" {
+		check.MissingField("client_secret")
+	}
+
+	if err := check.AsError(); err != nil {
+		return nil, err
+	}
+
+	cfg := &oauth2.Config{
+		RedirectURL:  callback,
+		ClientID:     id,
+		ClientSecret: secret,
+		Endpoint:     Endpoint,
+	}
+
+	return cfg, nil
 }
 
 // Empty() checks if Client credentials aren't set
