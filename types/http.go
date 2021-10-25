b7c35a5a2e93a22a7bbfc680d9e39a312ec89037
Alejandro Mery <amery@jpi.io>
2021-10-25 02:57:30 +0100

    types: introduce TransportUnwrapper{} interface and helper
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- types/http.go
+++ types/http.go
@@ -2,6 +2,9 @@ package types
 
 import (
 	"net/http"
+
+	"github.com/motemen/go-loghttp"
+	"golang.org/x/oauth2"
 )
 
 type HttpRequestDoer interface {
@@ -12,4 +15,37 @@ type HttpRequestDoerFunc func(*http.Request) (*http.Response, error)
 
 func (f HttpRequestDoerFunc) Do(req *http.Request) (*http.Response, error) {
 	return f(req)
+}
+
+type TransportUnwrapper interface {
+	Unwrap() *http.Transport
+}
+
+func TransportUnwrap(v interface{}) *http.Transport {
+	for v != nil {
+
+		switch t := v.(type) {
+		case *http.Transport:
+			// direct
+			return t
+		case TransportUnwrapper:
+			// semi-direct
+			return t.Unwrap()
+
+		case *http.Client:
+			// next http.RoundTripper
+			v = t.Transport
+		case *oauth2.Transport:
+			// next http.RoundTripper
+			v = t.Base
+		case *loghttp.Transport:
+			// next http.RoundTripper
+			v = t.Transport
+
+		default:
+			// give up
+			v = nil
+		}
+	}
+	return nil
 }
